903f4d961a0ab424572b9b693de703891209073f
Ryan Pflugradt <ryan.pflugradt@outlook.com>
2024-11-01 19:54:31 -0500

    add port to .env

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -11,8 +12,6 @@ import (
 	graph "github.com/rsp84/ride-with-me-backend/graph/generated"
 	"github.com/rsp84/ride-with-me-backend/graph/resolvers"
 )
-
-const defaultPort = "8080"
 
 func init() {
 	time.Local = time.UTC
@@ -29,6 +28,6 @@ func main() {
 	http.Handle("/graphql", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/graphql for GraphQL playground", defaultPort)
-	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
+	log.Printf("connect to http://localhost:%s/graphql for GraphQL playground", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
